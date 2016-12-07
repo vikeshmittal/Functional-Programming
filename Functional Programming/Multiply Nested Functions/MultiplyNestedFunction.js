d0db45c522fb4dcabde7ae93cb488f84df0f6e02
paribaranwal <paribaranwal@users.noreply.github.com>
2016-12-07 17:49:30 +0530

    Updating Multiply nested function example

--- Functional Programming/Multiply Nested Functions/MultiplyNestedFunction.js
+++ Functional Programming/Multiply Nested Functions/MultiplyNestedFunction.js
@@ -1,54 +1,14 @@
-(function(){
-	
-	var createShape = function(type) {
-		function createRectangle() {
-			var length, bredth;		
-			
-			return {
-				type: type,
-				configure: function(l, b) {
-					length = l;
-					bredth = b;
-				},
-				showConfiguration: function() {
-					return ("Length: " + length + " And Bredth: " + bredth);
-				},
-				area: function() {
-					return length * bredth;
-				}
-			};
+(function(){	
+	function goToFirstFloor(houseNo, floors){
+		function openMainDoor(){
+			function goToStaircase(floor){
+				console.log('You are at ' + floor + ' floor of house ' + houseNo);
+			}
+			for(var i=1; i <= floors; i++){
+				goToStaircase(i);
+			}
 		}
-		
-		function createSquare() {
-			var side;
-			
-			return {
-				type: type,
-				configure: function(s) {
-					side = s;
-				},
-				showConfiguration: function() {
-					return ("Side: " + side);
-				},
-				area: function() {
-					return side * side;
-				}
-			};
-		}
-		
-		
-		var shape = null;
-		if (type === 'rectangle') {
-			shape = createRectangle();
-		} else if (type === 'square') {
-			shape =  createSquare();
-		}
-		return shape;
-	};
-	var rectangle = createShape('rectangle'); 
-	var square = createShape('square'); 
-	rectangle.configure(2,3);
-	square.configure(5);
-	console.log(rectangle.type + " with " + rectangle.showConfiguration() + " has area of " + rectangle.area());
-	console.log(square.type + " with " + square.showConfiguration() + " has area of " + square.area());
+		openMainDoor(floors);
+	}
+	goToFirstFloor('H-34',2);
 }());
